d2328289943d1cfae670d990b81b673d865b33cf
Aryan Singh <aryansingh20nov@gmail.com>
2026-09-13 23:27:01 +0530

    test: deliberate checkstyle violation to prove gate

--- src/main/java/com/example/cicddemo/user/UserController.java
+++ src/main/java/com/example/cicddemo/user/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/v1/users")

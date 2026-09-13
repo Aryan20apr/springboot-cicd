d8f4b203e2687726d1aaa6c16e5670cef9171834
Aryan Singh <aryansingh20nov@gmail.com>
2026-09-13 23:28:55 +0530

    Revert "test: deliberate checkstyle violation to prove gate"
    This reverts commit d2328289943d1cfae670d990b81b673d865b33cf.

--- src/main/java/com/example/cicddemo/user/UserController.java
+++ src/main/java/com/example/cicddemo/user/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
